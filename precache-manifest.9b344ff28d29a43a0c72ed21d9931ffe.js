b23f56dd2dbf0db29f028aadee4bd8aeb46e875d
lmao <fireclimbers@gmail.com>
2019-12-06 15:44:46 -0800

    Updates

---
+++ precache-manifest.9b344ff28d29a43a0c72ed21d9931ffe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481c1ae591e602a0f02f798c98cc5806",
+    "url": "/macroman/index.html"
+  },
+  {
+    "revision": "63e9694e559e5f04f4e5",
+    "url": "/macroman/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5459165d0b80a2f1e99b",
+    "url": "/macroman/static/js/2.4f8b12ea.chunk.js"
+  },
+  {
+    "revision": "63e9694e559e5f04f4e5",
+    "url": "/macroman/static/js/main.e80747f7.chunk.js"
+  },
+  {
+    "revision": "04c6cf646ed2153ac028",
+    "url": "/macroman/static/js/runtime-main.9a0c6f56.js"
+  }
+]);
